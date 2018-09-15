09ecf0e778f9857856bcf4743bebd65e5b19fbdb
mpaezr <43300593+mpaezr@users.noreply.github.com>
2018-09-15 19:44:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 
 //conexio BDD
-$servidor="localhost";	
-$usuari="myforja";	
-$pass="jusg13PD";
-$db="forja";
+$servidor="SERVER";	
+$usuari="USER";	
+$pass="PASS";
+$db="BD";
 
 $bd = new mysqli($servidor,$usuari,$pass,$db);
 $bd->set_charset("utf8");
